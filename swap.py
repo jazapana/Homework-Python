b84469a820a360996c908723f378a6b2d5265a59
Joel Zapana <joel.zapana@sjsu.edu>
2021-10-09 19:53:02 -0700

    Finished creating the function, and added the necessary docstring/comments.

---
+++ swap.py
@@ -0,0 +1,9 @@
+def swap_last_item(myList):
+	''' Takes in a list, swaps the first and last element.
+		Returns the new list.
+	'''
+	# x will hold a copy of the last element, so it is not lost when I put first element in the last index.
+	x = myList[-1]
+	myList[-1] = myList[0]
+	myList[0] = x
+	return myList
