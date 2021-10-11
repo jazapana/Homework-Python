c41b7b5258aedf46d3686f953602a20918098637
Joel Zapana <joel.zapana@sjsu.edu>
2021-10-10 22:19:56 -0700

    Removed the wrapper from the decorator.

--- timing.py
+++ timing.py
@@ -1,13 +1,9 @@
 import time
 
 def calculate_time(func):
-	''' Takes in a function that will be used by the wrapper.
+	''' Takes in a function, saves the time that the function took to run.
+		Returns 'Total time X', X being the time.
 	'''
-	def wrapper():
-		''' Saves the time that the function took to run.
-			Returns 'Total time X', X being the time.
-		'''
-		# by using the time.time(), I can get the current time in seconds.
-		X = time.time()
-		print(f'Total time {X}')
-	return wrapper
+	# by using the time.time(), I can get the current time in seconds.
+	X = time.time()
+	print(f'Total time {X}')
