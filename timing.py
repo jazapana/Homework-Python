f834b0a682b4643e676ca5fafdfafc8c7dd64920
Joel Zapana <joel.zapana@sjsu.edu>
2021-10-12 11:39:18 -0700

    Changed the variable names of X, Y and Z, added more comments and reformatted the comments and docstrings.

--- timing.py
+++ timing.py
@@ -1,16 +1,20 @@
 import time
 
 def calculate_time(func):
-	''' Takes in a function that will be used by the wrapper.
-	'''
+	''' Takes in a function that will be used by the wrapper.'''
 	def wrapper():
 		''' Saves the time that the function took to run.
-			Returns 'Total time X', X being the time.
+		    Returns 'Total time X', X being the time.
 		'''
-		# by using the time.time(), I can get the current time in seconds.
-		X = time.time()
+		# By using the time.time(), I can get the current time in seconds.
+		A = time.time()
 		func()
-		Y = time.time()
-		Z = Y - X
-		print(f'Total time {Z}')
+		B = time.time()
+		'''
+		By storing the time in seconds before and after calling the function,
+		and by subtracting the time before from the time after, we can find the
+		total time that the function used.
+		'''
+		X = B - A
+		print(f'Total time {X}')
 	return wrapper
